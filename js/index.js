aeeb8875ed18cdd1f03baeae3ff1c77946cfbbad
sowmik-sec <sowmik.sec@gmail.com>
2023-02-06 22:23:37 +0600

    main body of news section added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-const loadCategories = () => {
-  fetch(`https://openapi.programming-hero.com/api/news/categories`)
-    .then((res) => res.json())
-    .then((data) => displayCategories(data.data.news_category));
+const loadCategories = async () => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/categories`
+  );
+  const data = await res.json();
+  displayCategories(data.data.news_category);
+  //displayCategories(data);
+  // .then((res) => res.json())
+  // .then((data) => displayCategories(data.data.news_category))
+  // .catch((error) => {
+  //   console.log(error);
+  // });
 };
 
 const displayCategories = (categories) => {
@@ -22,13 +30,65 @@ const clickHandleNews = (category_id) => {
 };
 
 const loadNewsCategory = (category_id) => {
-  fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+  fetch(
+    `https://openapi.programming-hero.com/api/news/category/${
+      "0" + category_id
+    }`
+  )
     .then((res) => res.json())
-    .then((data) => displayNewsCategory(data));
+    .then((data) => displayNewsCategory(data.data))
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const displayNewsCategory = (data) => {
-  console.log(data);
+  const newList = document.getElementById("show-news");
+  newList.innerHTML = ``;
+  data.forEach((item) => {
+    console.log(item);
+    const newsDiv = document.createElement("div");
+    newsDiv.innerHTML = `
+        <div class='d-flex justify-content-between mb-5'>
+            <div>
+                <img height='244' src='${item.image_url}'>
+            </div>
+            <div class='p-2 ms-2 d-flex flex-column justify-content-between'>
+                <div class='mb-2'>
+                    <h4>${item.title}</h4>
+                    <p>${item.details.slice(0, 250) + "..."}</p>
+                </div>
+                
+                <div class='d-flex justify-content-between'>
+                    <div class='d-flex'>
+                        <div>
+                            <img width='40' height='40' style='border-radius:50%' src='${
+                              item.author ? item.author.img : "Not Available"
+                            }'>
+                        </div>
+                        <div class='ms-2'>
+                            ${item.author ? item.author.name : "Not Available"}
+                            ${
+                              item.author.published_date
+                                ? item.author.published_date
+                                : "Not Available"
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <p>${
+                          item.total_view ? item.total_view : "Not Available"
+                        }</p>
+                    </div>
+                    <div>
+                        <p>${item.rating.number}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    newList.appendChild(newsDiv);
+  });
 };
 
 document.getElementById("blog").addEventListener("click", function () {
